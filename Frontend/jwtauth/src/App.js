5aa53bd6087409581fab6169854f29d248b8083d
dipakmp7 <dmpatil455@gmail.com>
2023-12-03 19:56:49 +0530

    added Home and Register components

--- Frontend/jwtauth/src/App.js
+++ Frontend/jwtauth/src/App.js
@@ -1,8 +1,18 @@
-
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Register from "./components/Register";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>Dipak</div>
+    // <div>Dipak</div>
+    < BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/jwtauth/src/components/Register.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Register(){
+    return(
+        <div>Dipak</div>
+    )
+}
+
+export default Register
