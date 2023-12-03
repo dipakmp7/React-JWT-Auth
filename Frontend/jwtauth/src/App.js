da14d5a8a2dd81f19ceff62243763ba66d39ec17
dipakmp7 <dmpatil455@gmail.com>
2023-12-03 20:32:03 +0530

    added routes in app.js

--- Frontend/jwtauth/src/App.js
+++ Frontend/jwtauth/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Register from "./components/Register";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
 
       </Routes>
     </BrowserRouter>
