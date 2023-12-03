2c1938532577983b3904faf8b313096402a02f02
dipakmp7 <dmpatil455@gmail.com>
2023-12-03 10:54:05 +0530

    modified index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,22 @@
-import { Express } from "express";
+import express from 'express';
+import mysql from 'mysql'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'manager',
+    database: 'jwtAuth'
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(2023,()=>{
+    console.log("server listen at 2023")
+})
