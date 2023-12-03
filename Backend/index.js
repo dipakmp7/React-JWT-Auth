3fc35721163a38ae9af4a62ed66b6d91dfe80b8d
dipakmp7 <dmpatil455@gmail.com>
2023-12-03 11:07:05 +0530

    added frontend dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -15,6 +15,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 
 app.listen(2023,()=>{
---
+++ Frontend/jwtauth/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <div>Dipak</div>
+  );
+}
+
+export default App;
