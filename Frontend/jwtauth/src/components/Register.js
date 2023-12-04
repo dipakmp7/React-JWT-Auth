1d7c710a050855d98f4b80547f734719c5d4bd3d
dipakmp7 <dmpatil455@gmail.com>
2023-12-04 17:59:15 +0530

    added register form

--- Frontend/jwtauth/src/components/Register.js
+++ Frontend/jwtauth/src/components/Register.js
@@ -2,7 +2,26 @@ import React from "react";
 
 function Register(){
     return(
-        <div>Dipak</div>
+        // <div>Dipak</div>
+        <div>
+            <form>
+                <div className="container">
+                    <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" placeholder="Enter Name"></input>
+                    </div>
+                    <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder="Enter Email"></input>
+                    </div>
+                    <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Enter Password"></input>
+                    </div>
+
+                </div>
+            </form>
+        </div>
     )
 }
 
