19747e20b9cf08e87bf69ddfa0bdb356defc527e
dipakmp7 <dmpatil455@gmail.com>
2023-12-05 11:18:01 +0530

    modify register page and index.js file

--- Frontend/jwtauth/src/components/Register.js
+++ Frontend/jwtauth/src/components/Register.js
@@ -1,26 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register(){
     return(
-        // <div>Dipak</div>
-        <div>
-            <form>
-                <div className="container">
-                    <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder="Enter Name"></input>
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Sign-Up Form</h2>
+                <form action="">
+                <div className="mb-3">
+                        <label htmlFor="name"><strong>Name</strong></label>
+                        <input type="text" name="name" placeholder="Enter Your Name" className="form-control rounded-0"></input>
                     </div>
-                    <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="Enter Email"></input>
+                    <div className="mb-3">
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" name="email" placeholder="Enter Your Email" className="form-control rounded-0"></input>
                     </div>
-                    <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter Password"></input>
+                    <div className="mb-3">
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" name="password" placeholder="Enter Your Password" className="form-control rounded-0"></input>
                     </div>
-
-                </div>
-            </form>
+                    <button type="submit" className="btn btn-success w-100">Sign Up</button>
+                    <p>You are agree to our term and policies</p>
+                    <Link to="/login" className="btn btn-default border w-100 bg-light">Login</Link>
+                </form>
+            </div>
         </div>
     )
 }
