806adeffcb0c2824fa129fa80b8a086f2f1fddc1
dipakmp7 <dmpatil455@gmail.com>
2023-12-07 11:34:53 +0530

    written a post api for backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mysql from 'mysql'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 
 const db = mysql.createConnection({
@@ -17,6 +19,18 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+
+app.post("/register",(req,res)=>{
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+
+    const sql = "insert into login(`name`,`email`,`password`)values(?)";
+
+    mysql.query(sql,[name,email,password],(result)=>{
+        console.log(res.result)
+    })
+})
 
 app.listen(2023,()=>{
     console.log("server listen at 2023")
